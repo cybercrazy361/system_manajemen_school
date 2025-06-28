a3b05e645fe553796a756eeb475ef4e32624e063
cybercrazy361 <kartiko.ardhi.361@gmail.com>
2025-06-28 23:30:47 +0700

    update

--- public/index.php
+++ public/index.php
@@ -1,24 +1,27 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
 // Maintenance mode handler
-if (file_exists($maintenance = __DIR__.'/../system_manajemen_school/storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader
 require __DIR__.'/vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request
-/** @var Application $app */
+// Bootstrap Laravel and get the application instance
 $app = require_once __DIR__.'/bootstrap/app.php';
 
-$app->handleRequest(
-    Request::capture()
+// Run the application
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
 );
 
+$response->send();
 
+$kernel->terminate($request, $response);
