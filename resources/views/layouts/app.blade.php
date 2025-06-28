6ebd6410c75cb114e4aa33b907b0557ccdcde555
cybercrazy361 <kartiko.ardhi.361@gmail.com>
2025-06-28 23:39:00 +0700

    update

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>@yield('title', 'SMA DHARMA KARYA')</title>
+    
+    <!-- FontAwesome CDN untuk ikon -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-papZc05hrZrMQnOYsl7AAO1uhRUQvpDdL/tvpm37gNfjG3WqkItdq0zTJdX7qMBsH0q43MXO3FzDscX/2xz8Ug==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    
+    <!-- CSS utama -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+    
+    @stack('styles')
+</head>
+<body>
+    @yield('content')
+
+    <!-- JS utama -->
+    <script src="{{ asset('js/app.js') }}"></script>
+
+    @stack('scripts')
+</body>
+</html>
